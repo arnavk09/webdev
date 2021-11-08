e31615d943e5d60214a56a29e4e35f53259f68d1
Arnav Katgeri <arnav02@outlook.com>
2021-11-08 16:50:46 +0530

    comments

--- backend/REST/project_three/index.js
+++ backend/REST/project_three/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+//start of important lines
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+//end of important lines. rest is usual stuff
 app.use(express.json());
 const port = 5000;
 app.get("/", (req, res) => {
@@ -9,7 +11,7 @@ app.get("/", (req, res) => {
 });
 //vscode.dev TEST
 app.post("/add", (req, res) => {
-  const { number1, number2 } = req.body;
+  const { number1, number2 } = req.body; //destructuring params
   var x = +number1 + +number2;
   res.send(`Sum : ${x}`);
 });
