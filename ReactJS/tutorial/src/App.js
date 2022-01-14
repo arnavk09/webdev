5a6f844abec08d2d2707f2e2ebb89c9f67324f85
Arnav Katgeri <arnav02@outlook.com>
2022-01-14 12:02:30 +0530

    testReact

--- ReactJS/tutorial/src/App.js
+++ ReactJS/tutorial/src/App.js
@@ -1,57 +1,32 @@
 import React from "react";
 import "./App.css";
+const author = "by Charlie Mackesy";
+console.log("initRender");
 //done upto jsx css 1:55:10 (video:https://www.youtube.com/watch?v=4UZrsTqkcW4)
 function App() {
   return (
     <>
       <section className="bookList">
         <Book />
+        <h4>{author.toUpperCase} </h4>
       </section>
     </>
   );
 }
 const Book = () => {
+  const title = "The Boy, The Mole, The Fox and The Horse";
+
   return (
     <>
       <article>
-        <Image />
-        <Title />
-        <Author />
+        <img
+          src="https://m.media-amazon.com/images/I/81PdkxXJObL._AC_UY327_FMwebp_QL65_.jpg"
+          alt="??"
+        />
+        <h1>{title}</h1>
       </article>
       <br />
-      <article>
-        <Image />
-        <Title />
-        <Author />
-      </article>
-      <br />
-      <article>
-        <Image />
-        <Title />
-        <Author />
-      </article>
-      <br />
-      <article>
-        <Image />
-        <Title />
-        <Author />
-      </article>
-      part
     </>
   );
-};
-const Image = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/I/81PdkxXJObL._AC_UY327_FMwebp_QL65_.jpg"
-      alt="??"
-    />
-  );
-};
-const Title = () => {
-  return <h1>The Boy, The Mole, The Fox and The Horse</h1>;
-};
-const Author = () => {
-  return <h4>by Charlie Mackesy </h4>;
 };
 export default App;
