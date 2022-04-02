f88b4c48462eaff5719c592a4d0c4ccea2f47609
Arnav Katgeri <arnav02@outlook.com>
2022-04-02 12:08:22 +0530

    re-learned

--- js/passengerCounter/index.js
+++ js/passengerCounter/index.js
@@ -2,5 +2,11 @@ var count = 0;
 var incrementButton = () => {
   count = count + 1;
   console.log(count);
+  var score=document.getElementById('incrementText')
+  score.innerHTML=count
+  // incrementText.innerHTML = count;
   return count;
+};
+var save = () => {
+  console.log("Saved as : "+count);
 };
